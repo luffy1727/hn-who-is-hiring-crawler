90764c8e371fb935212ff072f4822edd433083b3
luffy1727 <tushig.tushig@gmail.com>
2021-04-21 15:05:02 +0800

    feat: 🎸 modifying readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ## Basic overview
 This project is the first phase of my hackernews crawler.
-The main purpose of this project is to extract the data that is related to me, from the monthly hackernews hiring posts, and convert it into more user friendly interface.  
+The main purpose of this project is to automate the mundane task of browsing through hackernews job postings, and applying one by one.
 
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 ![Python](https://img.shields.io/badge/python-v3.6+-blue.svg)
@@ -19,17 +19,37 @@ pip3 install scrapy
 ```
 
 ## Usage
+Firstly, we crawl the data from hackernews and extract the ones that are related to me.
 #### Running the crawler
 ```python
 scrapy runspider crawler.py -o ../data/output.json
 ```
+Secondly, we clean the data and add weight to fields that corresponds more to my skillset.
 #### Running the cleaning script
 ```python
 python3 main.py
 ```
+#### How to get an app password
+In order to send an email from your email account, you need to acquire your app password from your google account.
+1. Go to manage my google account
+2. Under "Signing in to Google" confirm that "2-Step Verification" is "On" for the account.
+3. Also under "Signing in to Google" Select "App passwords".
+4. Select the app as "Mail" and the device as "Other (Custom name)" and name it.
+5. Copy the app password, it will be in a yellow box and looks like: "XXXX XXXX XXXX XXXX"
+6. Copy app password and email into email-credentials.json
+```bash
+cp email-credentials.json.dist email-credentials.json
+```
+#### Running the mailing service
+Lastly, we send our application with the attached resume to the extracted emails from the data if the job posting includes any email.
+
+```python
+python3 applier.py
+```
 
 #### Result
 ![Example data](example_json.png)
+![Result email](result.png)
 
 
 ## Contributing
--- applier.py
+++ applier.py
@@ -8,8 +8,10 @@ def apply():
         for data_item in data:
             if(data_item['email']):
                 subject = 'Job postings on Hackernews'
+                email = data_item['email']
                 position = data_item['position']
                 text = f'Greetings,\n\nThis letter is to express my interest in your posting on HackerNews for a {position} position.\n\nI am a junior backend developer at a fintech startup called AndSystems, where we developed an application that issues personal loans to under-served customers. As one of the lead developers who migrated the monolithic application into microservices, I have acquired hands-on experience with system design and AWS.\n\nYour listed requirements closely match my background and skills. A few I would like to highlight that would enable me to contribute to your bottom line are:\n\n- Highly skilled in designing, testing, and developing loosely coupled systems\n- Thorough understanding of AWS features\n- Hands-on experience with microservices\n- Passion for automation (CI/CD)\n- 3 year experience of writing production-level code\n\nI’ve attached a copy of my resume that includes the details of my projects and experience in software development.\n\nThank you for your time and consideration. I look forward to speaking with you about this opportunity.\n\nSincerely,\nChintushig Ochirsukh'
+                print(f'Sending an email to: {email}' )
                 email = data_item['email']
                 
                 send_email(subject, text, email)
@@ -27,7 +29,7 @@ def send_email(subject, text, email):
         msg.set_content(message)
         msg['Subject'] = subject
         msg['From'] = 'tushig.tushig@gmail.com'
-        msg['To'] = 'tushig.tushig@gmail.com'
+        msg['To'] = email
         with open('CHINTUSHIG_OCHIRSUKH_RESUME.pdf', 'rb') as content_file:
             content = content_file.read()
             msg.add_attachment(content, maintype='application', subtype='pdf', filename='CHINTUSHIG_OCHIRSUKH_RESUME.pdf')
